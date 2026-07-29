4c7418671958e5ee058918385e2b24c3af23c9ee
chubatien <chubatien1986@gmail.com>
2026-07-29 11:06:14 +0700

    Add files via upload

--- server.cjs
+++ server.cjs
@@ -4,6 +4,10 @@ var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
 var __copyProps = (to, from, except, desc) => {
   if (from && typeof from === "object" || typeof from === "function") {
     for (let key of __getOwnPropNames(from))
@@ -20,8 +24,14 @@ var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__ge
   isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
   mod
 ));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // server.ts
+var server_exports = {};
+__export(server_exports, {
+  default: () => server_default
+});
+module.exports = __toCommonJS(server_exports);
 var import_express = __toESM(require("express"), 1);
 var import_path = __toESM(require("path"), 1);
 var import_multer = __toESM(require("multer"), 1);
@@ -58,17 +68,17 @@ async function startServer() {
       const ai = new import_genai.GoogleGenAI({ apiKey });
       const prompt = `
 B\u1EA1n l\xE0 chuy\xEAn gia b\xF3c t\xE1ch d\u1EEF li\u1EC7u c\xE2u h\u1ECFi tr\u1EAFc nghi\u1EC7m t\u1EEB t\xE0i li\u1EC7u.
-Nhi\u1EC7m v\u1EE5 c\u1EE7a b\u1EA1n l\xE0 \u0111\u1ECDc to\xE0n b\u1ED9 \u0111o\u1EA1n v\u0103n b\u1EA3n sau v\xE0 tr\xEDch xu\u1EA5t t\u1EA5t c\u1EA3 c\xE1c c\xE2u h\u1ECFi tr\u1EAFc nghi\u1EC7m th\xE0nh danh s\xE1ch \u0111\u1ECBnh d\u1EA1ng JSON chu\u1EA9n.
+Nhi\u1EC7m v\u1EE5 c\u1EE7a b\u1EA1n l\xE0 \u0111\u1ECDc to\xE0n b\u1ED9 \u0111o\u1EA1n v\u0103n b\u1EA3n sau v\xE0 tr\xEDch xu\u1EA5t t\u1EA5t c\u1EA3 c\xE1c c\xE2u h\u1ECFi tr\u1EAFc nghi\u1EC7m th\xE0nh danh s\xE1ch \u0111\u1ECBnh d\u1EA1ng JSON chu\u1EA9n. Kh\xF4ng \u0111\u01B0\u1EE3c b\u1ECF s\xF3t hay gi\u1EDBi h\u1EA1n b\u1EA5t k\u1EF3 c\xE2u h\u1ECFi n\xE0o.
 
 M\u1ED7i c\xE2u h\u1ECFi ph\u1EA3i ch\u1EE9a c\xE1c th\xF4ng tin sau:
-- "content": N\u1ED9i dung \u0111\u1EA7y \u0111\u1EE7 c\u1EE7a c\xE2u h\u1ECFi (VD: "Th\u1EDDi h\u1EA1n t\u1ED5 ch\u1EE9c l\u1EC5 k\u1EBFt n\u1EA1p \u0111\u1EA3ng vi\xEAn theo Quy \u0111\u1ECBnh 20-Q\u0110/TW l\xE0 bao l\xE2u?")
+- "content": N\u1ED9i dung \u0111\u1EA7y \u0111\u1EE7 c\u1EE7a c\xE2u h\u1ECFi
 - "options": M\u1EA3ng 4 \u0111\xE1p \xE1n d\u1EA1ng ["A. N\u1ED9i dung A", "B. N\u1ED9i dung B", "C. N\u1ED9i dung C", "D. N\u1ED9i dung D"]
 - "correctAnswer": K\xFD t\u1EF1 \u0111\xE1p \xE1n \u0111\xFAng ("A", "B", "C", ho\u1EB7c "D")
 - "explanation": L\u1EDDi gi\u1EA3i th\xEDch ng\u1EAFn g\u1ECDn (n\u1EBFu c\xF3 trong \u0111\u1EC1 ho\u1EB7c t\u1EF1 t\xF3m t\u1EAFt ng\u1EAFn)
 
 V\u0103n b\u1EA3n \u0111\u1EA7u v\xE0o:
 ---
-${extractedText.substring(0, 15e3)}
+${extractedText.substring(0, 3e5)}
 ---
 
 Ch\u1EC9 tr\u1EA3 v\u1EC1 duy nh\u1EA5t chu\u1ED7i JSON c\xF3 c\u1EA5u tr\xFAc:
@@ -107,16 +117,20 @@ Ch\u1EC9 tr\u1EA3 v\u1EC1 duy nh\u1EA5t chu\u1ED7i JSON c\xF3 c\u1EA5u tr\xFAc:
       appType: "spa"
     });
     app.use(vite.middlewares);
-  } else {
+  } else if (!process.env.VERCEL) {
     const distPath = import_path.default.join(process.cwd(), "dist");
     app.use(import_express.default.static(distPath));
     app.get("*", (req, res) => {
       res.sendFile(import_path.default.join(distPath, "index.html"));
     });
   }
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-  });
+  if (!process.env.VERCEL) {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on http://0.0.0.0:${PORT}`);
+    });
+  }
+  return app;
 }
-startServer();
+var appPromise = startServer();
+var server_default = appPromise;
 //# sourceMappingURL=server.cjs.map
